aedd5c92be6a1d5ccc02308d2a7678f1c5be8cf6
lfhandfield <lfhandfield@gmail.com>
2020-03-06 09:30:12 +0000

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -9,11 +9,10 @@ library(stats)
 library(shinydashboard)
 library(shinyBS)
 
-
-ui <- dashboardPage(
- dashboardHeader(disable = T),
- dashboardSidebar(  shiny::tags$head(
-    shiny::tags$style(shiny::HTML("
+ui <- dashboardPage(dashboardHeader(disable = T),
+  dashboardSidebar(
+    shiny::tags$head(
+      shiny::tags$style(shiny::HTML("
                     body {
                     background-image: url('https://www.sanger.ac.uk/sites/default/files/wellcomesangerinstitutelogo1.png');
                     background-size: 200px;
@@ -32,51 +31,57 @@ ui <- dashboardPage(
                    #outertabBox {color: #000000; background-color: #888888;}  
                    #tabBox {color: #000000; background-color: #AAAAAA;}  
 
-                     "))), box(width=24,id = "outertabBox",tabBox(
-        width = 12,
-        id = "tabBox",
-        tabPanel("Select Table",fluidRow(
-     
-      selectInput(inputId = "dataset",
-            label = "Celltype and Sample calling:",
-            choices = c("Fine Celltypes / Multinomial" ,  "Broad Celltypes / Multinomial", "Scmap Celltypes / Multinomial" ,  "Fine Celltypes / Clustering" ,  "Broad Celltypes / Clustering", "Scmap Celltypes / Clustering" ),
-            selected = "Fine Celltypes / Multinomial"),
-      bsTooltip("dataset", "Choose a combinasion of Method for cell-type identification and Method for sample calling from citeseq tags",
-         "right", options = list(container = "body")), # ,  ,trigger="Click"
-      
-      selectInput(inputId = "resfield",
-            label = "Choose a result type:",
-            choices = c("gene" ,  "gene_preFDR", "consensus_gene" ,  "consensus_gene_preFDR", "go", "consensus_go"),
-            selected = "gene")
+                     ")
+        )
+    ), box(width=24,id = "outertabBox",tabBox(
+      width = 12,
+      id = "tabBox",
+      tabPanel("Select Table",fluidRow(
+        selectInput(inputId = "dataset",
+          label = "Celltype and Sample calling:",
+          choices = c("Fine Celltypes / Multinomial" ,  "Broad Celltypes / Multinomial", "Scmap Celltypes / Multinomial" ,  "Fine Celltypes / Clustering" ,  "Broad Celltypes / Clustering", "Scmap Celltypes / Clustering"),
+          selected = "gene"
+        ),
+        bsTooltip(
+          "dataset",
+          "Choose a combinasion of Method for cell-type identification and Method for sample calling from citeseq tags",
+          "right",
+          options = list(container = "body")
+        ),
+        selectInput(
+          inputId = "resfield",
+          label = "Choose a result type:",
+          choices = c("gene" ,  "gene_preFDR", "consensus_gene" ,  "consensus_gene_preFDR", "go", "consensus_go"),
+          selected = "gene"
+        )
       )),tabPanel("Table options",fluidRow(
-              selectInput(inputId = "obs",
-             label = "Extented Annotation:",
-            choices = c()),
-            bsTooltip("obs", "Displays an additionnal annotation below the table when a row is selected", "right", options = list(container = "body")),
-            checkboxGroupInput("showCols", "Visible Columns:",c(), selected=c())
-    #  selectizeInput( "filterchoice", "Choose Value:", choices = NULL, options = list(create = TRUE)),
+        selectInput(inputId = "obs",
+          label = "Extented Annotation:",
+          choices = c()
+        ),
+        bsTooltip("obs", "Displays an additionnal annotation below the table when a row is selected", "right", options = list(container = "body")),
+        checkboxGroupInput("showCols", "Visible Columns:",c(), selected=c())
       )),tabPanel("Heatmap options",fluidRow(
         selectInput(inputId = "comtype",
           label = "Heatmap includes:",
           choices = c("All", "Paired Comparisons", "Pooled Comparisons")),
         bsTooltip("comtype", "Filters some collumn on heatmap, containing comparison solely based on 1vs1 sample comparison and/or pooled 2vs2 samples comparisons", "right", options = list(container = "body")),
-        sliderInput("nbhistcols", label = "Nb colunm for histogram:",
-                  min = 3, max = 100, value = 15),
-        bsTooltip("nbhistcols", "Maximum number of collumn displayed in heatmap",
-         "right", options = list(container = "body"))
+        sliderInput("nbhistcols", label = "Nb colunm for histogram:", min = 3, max = 100, value = 15),
+        bsTooltip("nbhistcols", "Maximum number of collumn displayed in heatmap", "right", options = list(container = "body"))
       )),tabPanel("Filters",fluidRow(
-          selectInput(inputId = "filter",
-                  label = "Filter Field:",
-                  choices = c("Comparison" ,  "Celltype", "NAME"),
-                  selected = "Comparison"),
-            selectInput(inputId = "filterchoice",
-            label = "Filter Value:",
-            choices = NULL),
-  
+        selectInput(inputId = "filter",
+          label = "Filter Field:",
+          choices = c("Comparison" ,  "Celltype", "NAME"),
+          selected = "Comparison"
+        ),
+        selectInput(inputId = "filterchoice", label = "Filter Value:", choices = NULL),
         actionButton(inputId = "fltaddbutton",label = "Add/Remove Filter"),
-        bsTooltip("fltaddbutton", "Add Filter, or remove filter if it is already present or if rows from displayed filter are selected", "right", options = list(container = "body"))
-    
-))))
+        bsTooltip("fltaddbutton",
+          "Add Filter, or remove filter if it is already present or if rows from displayed filter are selected",
+          "right",
+          options = list(container = "body")
+        )
+  ))))
 ), dashboardBody(
       shiny::tags$h4(uiOutput("selectedQuery")),
       dataTableOutput("currentfilters"),
@@ -85,87 +90,4 @@ ui <- dashboardPage(
       uiOutput("help2"),
       plotOutput("map")
       )
-)
-library(stats)
-
-# mount-farm is required to access /lustre
-# Define server logic to summarize and view selected dataset ----
-ui <- fluidPage(
-  useShinyjs(),
-#  extendShinyjs(text = "shinyjs.hideFilt = document.getElementById('currentfilters').style.display='block'", functions=c("hideFilt")),
-#  extendShinyjs(text = "shinyjs.showFilt = document.getElementById('currentfilters').style.display='none'" , functions=c("showFilt")),
-  shiny::tags$head(
-    shiny::tags$style(shiny::HTML("
-                    body {
-                    background-image: url('https://www.sanger.ac.uk/sites/default/files/wellcomesangerinstitutelogo1.png');
-                    background-size: 200px;
-                    background-attachment: fixed;
-                    background-repeat: no-repeat;
-                    background-position: center;
-                    }"))),
-  
-  # App title ----
-  titlePanel("Browse DE genes"),
-
-  # Sidebar layout with a input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: Selector for choosing dataset ----
-      selectInput(inputId = "dataset",
-            label = "Choose a dataset:",
-            choices = c("MH" ,  "MHBR", "JaJn" ,  "MHS", "MHBRS", "JaJnS"),
-            selected = "gene"),
-      
-      selectInput(inputId = "resfield",
-            label = "Choose a result type:",
-            choices = c("gene" ,  "gene_preFDR", "consensus_gene" ,  "consensus_gene_preFDR", "go", "consensus_go"),
-            selected = "gene"),
-      selectInput(inputId = "obs",
-             label = "Extented Annotation:",
-            choices = c()),
-
-      selectInput(inputId = "filter",
-                  label = "Filter Field:",
-                  choices = c("Comparison" ,  "Celltype", "NAME"),
-                  selected = "Comparison"),
-    #  selectizeInput( "filterchoice", "Choose Value:", choices = NULL, options = list(create = TRUE)),
-      selectInput(inputId = "filterchoice",
-            label = "Filter Value to Add:",
-            choices = NULL),
-
-    
-      # Input: Numeric entry for number of obs to view ----
-      actionButton(inputId = "fltaddbutton",label = "Add/Remove Filter"),
-      
-      checkboxGroupInput("showCols", "Visible Columns:",c(), selected=c()),
-      selectInput(inputId = "comtype",
-        label = "Heatmap includes:",
-        choices = c("All", "Paired Comparisons", "Pooled Comparisons")),
-      sliderInput("nbhistcols", label = "Nb colunm for histogram:",
-                  min = 3, max = 100, value = 15)
-    ),
-    
-    
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Verbatim text for data summary ----
-      #verbatimTextOutput("summary"),
-
-      # Output: HTML table with requested number of observations ----
-      #tableOutput("view"),
-      shiny::tags$h4(uiOutput("selectedQuery")),
-      dataTableOutput("currentfilters"),
-      dataTableOutput("results"),
-      uiOutput("help"),
-      uiOutput("help2"),
-      plotOutput("map")
-      
-      #webGLOutput("myWebGL")
-    )
-  )
 )
