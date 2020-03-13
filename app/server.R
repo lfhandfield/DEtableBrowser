e7b79fda4f165ec213c60291af5fa7be68487412
lfhandfield <lfhandfield@gmail.com>
2020-03-13 23:12:22 +0000

    Update server.R

--- app/server.R
+++ app/server.R
@@ -549,12 +549,12 @@ flt[is.na(flt)] <- FALSE
                   if (sum(fltrow) < 120) lengthlist = c(lengthlist[lengthlist < sum(fltrow)], sum(fltrow))
                   
                   defsort <- switch(simplesort(), c(NA, NA),"pfc" = c(match("Log2FC", input$showCols), "desc"), "nfc" = c(match("Log2FC", input$showCols), "asc"), "signif"= c(match("DEseq_adj_Log10pval", input$showCols), "asc"))
-                  if (is.na(defsort[[1]])) defsort <- c()
-                  else defsort[[1]] <- defsort[[1]] + 1
+		  if (is.na(defsort[1])) defsort <- c()
+                  else defsort[1] <- defsort[1] + 1
                   datatable(data()[fltrow,input$showCols], selection = 'single',
                             #options = list(columnDefs = list(list(width = '70px', targets = c(2, 3, 4)), list(width = '10px', targets = c(0))), pageLength = 5, autoWidth = TRUE, dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel')),
                             extensions = 'Scroller', colnames = input$showCols,
-                           options = list(dom = 'lpt', stateSave=T, lengthMenu = lengthlist, order = defsort),
+                           options = list(dom = 'lpt', stateSave=T, lengthMenu = lengthlist, order = list(defsort)),
                           rownames = F)
                   }
                 }
