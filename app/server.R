a8296233083111ec7d9e73bab04516ab7ef0005a
lfhandfield <lfhandfield@gmail.com>
2020-03-14 08:21:17 +0000

    Update server.R

--- app/server.R
+++ app/server.R
@@ -544,14 +544,15 @@ flt[is.na(flt)] <- FALSE
                   lengthlist = c(5, 10, 15, 20, 25, 30, 40, 50, 60, 80, 100, 120)
                   if (sum(fltrow) < 120) lengthlist = c(lengthlist[lengthlist < sum(fltrow)], sum(fltrow))
                   
-		  defsort <- switch(simplesort(), c(NA, NA),"pfc" = c(match("Log2FC", input$showCols), "desc"), "nfc" = c(match("Log2FC", input$showCols), "asc"), "signif"= c(match("DEseq_adj_Log10pval", input$showCols), "asc"))
-                  if (is.na(defsort[1])) defsort <- c()
-                  else defsort[1] <- as.numeric(defsort[1]) - 1
- 
+		  #defsort <- switch(simplesort(), c(NA, NA),"pfc" = c(match("Log2FC", input$showCols), "desc"), "nfc" = c(match("Log2FC", input$showCols), "asc"), "signif"= c(match("DEseq_adj_Log10pval", input$showCols), "asc"))
+                  #if (is.na(defsort[1])) defsort <- c()
+                  #else defsort[1] <- as.numeric(defsort[1]) - 1
+ 		  #,order = list(defsort)
+			
                   datatable(data()[fltrow,input$showCols], selection = 'single',
                             #options = list(columnDefs = list(list(width = '70px', targets = c(2, 3, 4)), list(width = '10px', targets = c(0))), pageLength = 5, autoWidth = TRUE, dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel')),
                             extensions = 'Scroller', colnames = input$showCols,
-                           options = list(dom = 'lpt', stateSave=T, lengthMenu = lengthlist,order = list(defsort)),
+                           options = list(dom = 'lpt', stateSave=T, lengthMenu = lengthlist),
                           rownames = F)
                   }
                 }
