c1ed4316f2cd846947d573e97c296088c77b4ec5
lfhandfield <lfhandfield@gmail.com>
2020-03-14 11:03:42 +0000

    Update server.R

--- app/server.R
+++ app/server.R
@@ -443,13 +443,22 @@ flt[is.na(flt)] <- FALSE
   
   observe({
   output$map <- renderPlot({
-    print(plotgenes())
       if (length(plotgenes()) > 1){
             curcolnames <- colnames(mat()$deseq$logpval)
             if (input$comtype == "All") colfilt <- rep(T, length(curcolnames))
             else if (input$comtype == "Pooled Comparisons") colfilt <- mat()$ispool[mat()$coltotest]
             else colfilt <- !mat()$ispool[mat()$coltotest]
-            print(match(plotgenes(),rownames(mat()$deseq$logpval)))
+            
+            if (input$ctpexcl == "All but Microglia") tmp <- grepl("[Mm]icroglia", mat()$celltype)
+            else if (input$ctpexcl == "All but Neurons") tmp <- grepl("[Nn]euron", mat()$celltype)
+            else if (input$ctpexcl == "All but Microglia or Neurons") tmp <- grepl("[Nn]euron", mat()$celltype) | grepl("[Mm]icroglia", mat()$celltype)
+            }else if (input$ctpexcl == "Match Filters"){
+              tmp <- match("Celltype", rownames(curflt()))
+              if (is.na(tmp)) tmp <- rep(T, length(mat()$celltype))
+              else tmp <- !is.na(match(mat()$celltype , strsplit(curflt()$value[tmp] , "[[:space:]];[[:space:]]")[[1]]))
+            }else tmp <- rep(T, length(mat()$celltype))
+            
+            colfilt <- colfilt & tmp[mat()$coltoct]
             names(colfilt) <- NULL
 
             colselect <- order(colSums(mat()$deseq$logpval[plotgenes(),colfilt,drop=F] < -1.3), decreasing=T)
@@ -475,6 +484,7 @@ flt[is.na(flt)] <- FALSE
           c1mat <- matrix("#AAAAAA", nrow= length(rnam), ncol = length(cnam), dimnames = list(rnam,cnam))
           c2mat <- matrix("#888888", nrow= length(rnam), ncol = length(cnam), dimnames = list(rnam,cnam))
 
+
           for(j in 1:length(cnam)) {
             for(i in 1:length(rnam)) {
             k <- match(paste(rnam[i], cnam[j],sep="_"), colnames(mat()$deseq$log2FC))
@@ -485,6 +495,7 @@ flt[is.na(flt)] <- FALSE
             c1mat[,j] <- rep(mat()$colA[j], nrow(c1mat))
             c2mat[,j] <- rep(mat()$colB[j], nrow(c1mat)) 
           }
+
           plotDataGrid(list(data = dmat , w=wmat, c1 = c1mat, c2 = c2mat), transform=list(w="log10pval"))
       }
   }, height = 300 + ifelse(length(plotgenes()) == 1, length(unique(data()[["Celltype"]])) , length(plotgenes()))* 24)
@@ -544,15 +555,14 @@ flt[is.na(flt)] <- FALSE
                   lengthlist = c(5, 10, 15, 20, 25, 30, 40, 50, 60, 80, 100, 120)
                   if (sum(fltrow) < 120) lengthlist = c(lengthlist[lengthlist < sum(fltrow)], sum(fltrow))
                   
-		  #defsort <- switch(simplesort(), c(NA, NA),"pfc" = c(match("Log2FC", input$showCols), "desc"), "nfc" = c(match("Log2FC", input$showCols), "asc"), "signif"= c(match("DEseq_adj_Log10pval", input$showCols), "asc"))
-                  #if (is.na(defsort[1])) defsort <- c()
-                  #else defsort[1] <- as.numeric(defsort[1]) - 1
- 		  #,order = list(defsort)
-			
+                  defsort <- switch(simplesort(), c(NA, NA),"pfc" = c(match("Log2FC", input$showCols), "desc"), "nfc" = c(match("Log2FC", input$showCols), "asc"), "signif"= c(match("DEseq_adj_Log10pval", input$showCols), "asc"))
+                  if (is.na(defsort[1])) defsort <- c()
+                  else defsort[1] <- as.numeric(defsort[1]) - 1
+                  # 
                   datatable(data()[fltrow,input$showCols], selection = 'single',
                             #options = list(columnDefs = list(list(width = '70px', targets = c(2, 3, 4)), list(width = '10px', targets = c(0))), pageLength = 5, autoWidth = TRUE, dom = 'Bfrtip', buttons = c('copy', 'csv', 'excel')),
                             extensions = 'Scroller', colnames = input$showCols,
-                           options = list(dom = 'lpt', stateSave=T, lengthMenu = lengthlist),
+                           options = list(dom = 'lpt', stateSave=T, lengthMenu = lengthlist,order = list(defsort)),
                           rownames = F)
                   }
                 }
