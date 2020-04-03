21746fb15547e95dff1dabb5152d4789e71178db
lfhandfield <lfhandfield@gmail.com>
2020-04-03 12:10:46 +0100

    Update render.R

--- app/render.R
+++ app/render.R
@@ -60,18 +60,18 @@ daccrange <- colorRampPalette(c("#00FFFF", "#00B0FF","#0079FF","#0000E8", "#0000
 #  for(flist in 1:length(compset)){
     gdata <- data.frame(row.names = rownames(overdata$coords))
     gdata$X <- overdata$coords[,1]; gdata$Y <- overdata$coords[,2]
-    tmp <- overdata$partition@.Data
-    tmp <- overdata$dematrices[[gene]][tmp,compset[flist]] 
-    tmp[(!overdata$dropout[, gene]) ] <- NA
+#    tmp <- overdata$partition@.Data
+#    tmp <- overdata$dematrices[[gene]][tmp,compset[flist]] 
+#    tmp[(!overdata$dropout[, gene]) ] <- NA
 
-    sampleset <- which(overdata[,compset[[flist]]])
-    bg <- !(overdata$sample %in% sampleset)
-    tmp[bg] <- NA
-    gdata$C <- tmp
+#    sampleset <- which(overdata[,compset[[flist]]])
+#    bg <- !(overdata$sample %in% sampleset)
+#    tmp[bg] <- NA
+#    gdata$C <- tmp
   
-    p <- ggplot(gdata, aes(x=X,y=Y,fill=C, alpha=C)) + geom_point();
-    p <- p + scale_color_gradientn(name=transform,colours=colpal, na.value= "#BBBBBB")
-    p <- p + scale_alpha_continuous(position=NULL,guide="none", na.value=0.25, range = c(1, 1))
+    p <- ggplot(gdata, aes(x=X,y=Y)) + geom_point();
+#   ,fill=C, alpha=C p <- p + scale_color_gradientn(name=transform,colours=colpal, na.value= "#BBBBBB")
+#    p <- p + scale_alpha_continuous(position=NULL,guide="none", na.value=0.25, range = c(1, 1))
     return(changeStyle(p, list(title=gene)))}
  #   gglist <- c(gglist,changeStyle(p, list(title=gene))) 
 #  }
