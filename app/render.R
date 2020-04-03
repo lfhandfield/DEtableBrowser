7cda092a01d6961becb0e65267eeee2da8d47303
lfhandfield <lfhandfield@gmail.com>
2020-04-03 13:58:34 +0100

    Update render.R

--- app/render.R
+++ app/render.R
@@ -59,7 +59,6 @@ daccrange <- colorRampPalette(c("#00FFFF","#00B0FF","#0079FF","#0000E8","#000074
     gdata <- data.frame(row.names = rownames(overdata$coords))
     gdata$X <- overdata$coords[,1]; gdata$Y <- overdata$coords[,2]
     frange <- overdata$dematrices[[gene]][,compset[flist]]
-    return(plot(1:3,1:3))
     frange[frange < aurange[1]] <- aurange[1]; frange[frange > aurange[2]] <- aurange[2]
     tmp <- frange[overdata$partition@.Data]
     #tmp[(!overdata$dropout[, gene]) ] <- NA
@@ -68,6 +67,7 @@ daccrange <- colorRampPalette(c("#00FFFF","#00B0FF","#0079FF","#0000E8","#000074
 #    bg <- !(overdata$sample %in% sampleset)
 #    tmp[bg] <- NA
     gdata$C <- tmp
+    return(plot(1:3,1:3))
   
     p <- ggplot(gdata, aes(x=X,y=Y,color=C, alpha=C)) + geom_point();
     p <- p + scale_color_gradientn(name=transform,colours=daccrange, na.value= "#BBBBBB")
