ed9e29bfad737e7845a3481c63b49b9f98a7d3d0
lfhandfield <lfhandfield@gmail.com>
2020-04-23 18:07:35 +0100

    Update server.R

--- app/server.R
+++ app/server.R
@@ -389,16 +389,20 @@ server <- function(input, output, session) {
             c1mat <- matrix("#AAAAAA", nrow= length(plotgenes()), ncol = length(colselect))
             c2mat <- c1mat
             
+            override <- list(top = rep("", length(colselect)), bot = rep("", length(colselect)))
             whiteCMP <- rgb(col2rgb(mat()$color_CMP)/1020 + 0.75)
             whiteCT <- rgb(col2rgb(mat()$color_CT)/1020 + 0.75)
             colcolors <- rep("#AAAAAA", length(colselect)) 
             for(j in 1:length(colselect)) {
+              override$top[j] <- mat()$celltypes[mat()$coltoct[colselect[j]]]
+              override$bot[j] <- mat()$comp_titles[mat()$coltotest[colselect[j]]]
               c1mat[,j] <- rep(whiteCMP[mat()$coltotest[colselect[j]]], nrow(c1mat))
               c2mat[,j] <- rep(whiteCT[mat()$coltoct[colselect[j]]], nrow(c1mat)) 
               colcolors[j] <- mat()$color_CT[mat()$coltoct[colselect[j]]]
             }
             #value(c(c1mat[1,], c2mat[1,]))
-            return(plotDataGrid(list(data = mat()$deseq$log2FC[plotgenes(),colselect,drop=F], w=mat()$deseq$logpval[plotgenes(),colselect,drop=F], c1 = c1mat, c2 = c2mat), colcolors = colcolors, do.cluster = c(input$clusterheat %in% c("Cluster Genes","Cluster Both"),input$clusterheat %in% c("Cluster Columns","Cluster Both")), transform=list(w="log10pval"),plot.attribs =list(xlabel = "Cell-type x Comparison", ylabel= "Genes")))
+            
+            return(plotDataGrid(list(data = mat()$deseq$log2FC[plotgenes(),colselect,drop=F], w=mat()$deseq$logpval[plotgenes(),colselect,drop=F], c1 = c1mat, c2 = c2mat), colcolors = colcolors, do.cluster = c(input$clusterheat %in% c("Cluster Genes","Cluster Both"),input$clusterheat %in% c("Cluster Columns","Cluster Both")), transform=list(w="log10pval"), override.colnames = override, plot.attribs =list(xlabel = "Cell-type x Comparison", ylabel= "Genes")))
       }else if ((length(plotgenes()) == 0)||(! plotgenes() %in% rownames(mat()$deseq$logpval))) ggplot()
       else {#comtype
           rnam = mat()$celltypes
