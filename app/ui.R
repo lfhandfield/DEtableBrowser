702a2a129bb5e870519d54cad02ab9b46de6d14c
lfhandfield <lfhandfield@gmail.com>
2020-04-24 12:14:50 +0100

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -115,14 +115,14 @@ ui <- dashboardPage(dashboardHeader(disable = T),
               choices = c("All", "1-to-1 Comparisons", "Pooled Comparisons"), selected = "1-to-1 Comparisons"),
   bsTooltip("comtype", "Filters columns on heatmap based on whether 1-to-1 sample comparison and/or pooled 2vs2 samples comparisons are performed", "right", options = list(container = "body")),
   selectInput(inputId = "samexcl", label = "Sample Included:",
-              choices = c("Match ConsensusGroup","Match Comparison","point-mutation conditions", "other disease conditions", "other neutral conditions","Include All"), selected = "point-mutation conditions"),
+              choices = c("Match ConsensusGroup","Match Comparison","point-mutation conditions", "other disease conditions", "other neutral conditions","Include All"), selected = "Match ConsensusGroup"),
   bsTooltip("samexcl", "Filters columns corresponding to condition tested. May match filters used for the table.", "right", options = list(container = "body")),
   selectInput(inputId = "ctpexcl", label = "Celltype Included:",
               choices = c("Microglia", "Neurons", "Microglia and Neurons","Match Filters","All"), selected = "Match Filters"),
   bsTooltip("ctpexcl", "Filters columns corresponding to celltype from each organoid in which fold change expression is reported on the heatmap", "right", options = list(container = "body")),
   selectInput(inputId = "clusterheat", label = "rows & cols clustering:",
               choices = c("No ordering", "Cluster Genes", "Cluster Columns","Cluster Both"), selected = "No ordering"),
-  sliderInput("nbhistcols", label = "Nb column for histogram:", min = 3, max = 100, value = 30),
+  sliderInput("nbhistcols", label = "Max Nb of columns:", min = 3, max = 100, value = 30),
   bsTooltip("nbhistcols", "Maximum number of collumn displayed in heatmap", "right", options = list(container = "body"))
 
 
